97483f25d0cc97c9abf1af4655a650d037b181a7
Sandra Madarnas <sandra.madarnas.1@gmail.com>
2023-02-13 22:09:32 +0000

    Primeros pasos para crear la aplicación de Random Card Generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// const Body = document.querySelector("body");
+let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+let suits = ["♠", "♣", "♥", "♦"];
+
+function getRandomCard() {
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+  const number = numbers[Math.floor(Math.random() * numbers.length)];
+  let card = number + "   " + suit;
+
+  if (suit === "♥" || suit === "♦") {
+    card = "<span class='red'>" + card + "</span>";
+  }
+
+  return card;
+}
+
+const button = document.querySelector("button");
+const display = document.querySelector("#display");
+
+button.addEventListener("click", function() {
+  const card = getRandomCard();
+  // display.textContent = card;
+  display.innerHTML = card;
+});
